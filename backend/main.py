d3e20843a7cbfa71cc5e89ffce76b5691705ea7e
Milos Svana <milos.svana@mailfence.com>
2025-03-26 21:03:02 +0100

    Add rate limiter and CORS

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,18 @@
 import flask
+from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from sentence_transformers import SentenceTransformer
 
-app = flask.Flask(__name__)
 model = SentenceTransformer("all-MiniLM-L6-v2")  # type: ignore
+
+app = flask.Flask(__name__)
+CORS(app, origins=["https://embeddings.svana.name", "http://127.0.0.1:5173"])
+limiter = Limiter(get_remote_address, app=app)
 
 
 @app.route("/embeddings", methods=["POST"])
+@limiter.limit("30 per minute")
 def embeddings():
     post_data = flask.request.get_json()
     inputs = post_data.get("inputs", [])
