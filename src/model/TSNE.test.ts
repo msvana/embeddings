df7754fd1a155b59fc42fb23cdb318af3c0cb742
Milos Svana <milos.svana@mailfence.com>
2025-03-29 20:39:50 +0100

    Implement basic transformation

--- src/model/TSNE.test.ts
+++ src/model/TSNE.test.ts
@@ -1,17 +1,46 @@
 import { test, expect } from "vitest";
 import * as TSNE from "./TSNE.ts";
 
-test("calculates affinities properly", () => {
-    const X: number[][] = [
-        [1, 2, 3],
-        [2, 3, 1],
-        [2, 2, 2],
-        [1, 2, 1],
-        [3, 2, 3],
-        [3, 0, 3],
-    ];
+const X: number[][] = [
+    [1, 2, 3],
+    [2, 3, 1],
+    [2, 2, 2],
+    [1, 2, 1],
+    [3, 2, 3],
+    [3, 0, 3],
+];
 
-    const affinities = TSNE.calculateAffinities(X, 1);
+test("calculates affinities properly", () => {
+    const sigmas = new Array(X.length).fill(1);
+    const affinities = TSNE.calculateAffinities(X, sigmas);
+    expect(affinities.length).to.equal(6);
+
+    for (let i = 0; i < affinities.length; i++) {
+        let sum = 0;
+
+        for (let j = 0; j < affinities.length; j++) {
+            sum += affinities[i][j];
+        }
+
+        expect(Math.abs(sum - 1)).to.lessThanOrEqual(1e-6);
+    }
+});
+
+test("calculates symmetric affinities properly", () => {
+    const sigmas = [0.5, 1.0, 1.5, 2.0, 2.5, 3.0];
+    const symmetricAffinities = TSNE.calculateSymmetricAffinities(X, sigmas);
+
+    for (let i = 0; i < X.length; i++) {
+        expect(symmetricAffinities[i][i]).to.equal(0);
+
+        for (let j = i; j < X.length; j++) {
+            expect(symmetricAffinities[i][j]).to.equal(symmetricAffinities[j][i]);
+        }
+    }
+});
+
+test("calculates projected affinities properly", () => {
+    const affinities = TSNE.calculateProjectedAffinity(X);
     expect(affinities.length).to.equal(6);
 
     for (let i = 0; i < affinities.length; i++) {
@@ -34,7 +63,6 @@ test("calculates perplexity correctly", () => {
     ];
 
     const perplexity = TSNE.calculatePerplexity(affinities[0]);
-    console.log(perplexity);
     expect(Math.abs(perplexity - 2.971)).to.lessThan(1e-4);
 });
 
@@ -51,19 +79,36 @@ test("calculates sigma upper bound properly", () => {
 });
 
 test("finds best sigmas correctly", () => {
-    const X: number[][] = [
-        [1, 2, 3],
-        [2, 3, 1],
-        [2, 2, 2],
-        [1, 2, 1],
-        [3, 2, 3],
-        [3, 0, 3],
-    ];
-
     const sigmas = TSNE.findBestSigmas(X, 3);
 
     for (let i = 0; i < X.length; i++) {
         expect(sigmas[i]).to.lessThan(2);
         expect(sigmas[i]).to.greaterThan(0);
+    }
+});
+
+test("initializes random solution properly", () => {
+    const Y = TSNE.initRandomProjection(10, 2);
+
+    expect(Y.length).to.equal(10);
+
+    for (const y of Y) {
+        expect(y.length).to.equal(2);
+        expect(Math.abs(y[0])).to.lessThanOrEqual(1e-2);
+        expect(Math.abs(y[1])).to.lessThanOrEqual(1e-2);
+    }
+});
+
+test("entire transformation works", () => {
+    const Y = TSNE.transform(X, 2, 4);
+
+    for (const y of Y) {
+        let row = "";
+
+        for (const n of y) {
+            row += `${n.toFixed(3)} `;
+        }
+
+        // console.log(row);
     }
 });
--- src/model/TSNE.ts
+++ src/model/TSNE.ts
@@ -1,20 +1,68 @@
-export function calculateAffinities(X: number[][], sigma: number): number[][] {
-    const affinities = new Array(X.length);
+export function transform(
+    X: number[][],
+    nDims: number,
+    perp: number = 5,
+    nIter: number = 100,
+): number[][] {
+    const sigmas = findBestSigmas(X, perp);
+    const affinities = calculateSymmetricAffinities(X, sigmas);
+    let Y = initRandomProjection(X.length, nDims);
+    let YPrev = structuredClone(Y);
 
-    for (let i = 0; i < affinities.length; i++) {
-        affinities[i] = new Array(X.length).fill(0);
+    for (let i = 0; i < nIter; i++) {
+        const gradient = calculateGradient(affinities, Y);
+        const YNew = new Array(Y.length);
+
+        for (let j = 0; j < Y.length; j++) {
+            YNew[j] = new Array(Y[j].length);
+
+            for (let k = 0; k < Y[j].length; k++) {
+                YNew[j][k] = Y[j][k] + gradient[j][k] * 1e-4;
+            }
+        }
+
+        YPrev = Y;
+        Y = YNew;
     }
 
-    const affinitySums = new Array(X.length).fill(0);
+    return Y;
+}
+
+export function calculateSymmetricAffinities(X: number[][], sigmas: number[]) {
+    const affinities = calculateAffinities(X, sigmas);
+    const symmetricAffinities = new Array(X.length);
+
+    for (let i = 0; i < X.length; i++) {
+        symmetricAffinities[i] = new Array(X.length).fill(0);
+    }
 
     for (let i = 0; i < X.length; i++) {
         for (let j = i + 1; j < X.length; j++) {
+            const p = (affinities[i][j] + affinities[j][i]) / (2 * X.length);
+            symmetricAffinities[i][j] = p;
+            symmetricAffinities[j][i] = p;
+        }
+    }
+
+    return symmetricAffinities;
+}
+
+export function calculateAffinities(X: number[][], sigmas: number[]): number[][] {
+    const affinities = new Array(X.length);
+    const affinitySums = new Array(X.length).fill(0);
+
+    for (let i = 0; i < X.length; i++) {
+        affinities[i] = new Array(X.length).fill(0);
+
+        for (let j = 0; j < X.length; j++) {
+            if (i === j) {
+                continue;
+            }
+
             const distance = euclideanDistanceSquared(X[i], X[j]);
-            const affinity = Math.exp(-distance / (2 * sigma * sigma));
+            const affinity = Math.exp(-distance / (2 * sigmas[i] * sigmas[i]));
             affinities[i][j] = affinity;
-            affinities[j][i] = affinity;
             affinitySums[i] += affinity;
-            affinitySums[j] += affinity;
         }
     }
 
@@ -28,13 +76,13 @@ export function calculateAffinities(X: number[][], sigma: number): number[][] {
 }
 
 export function findBestSigmas(X: number[][], perp: number, maxIterations: number = 50): number[] {
-    const lowerBounds = new Array(X.length).fill(1e-6);
+    const lowerBounds = new Array(X.length).fill(1e-3);
     const upperBounds = new Array(X.length).fill(calculateInitialUpperBound(X));
     const sigmas = new Array(X.length).fill((upperBounds[0] + lowerBounds[0]) / 2);
 
     for (let i = 0; i < sigmas.length; i++) {
         for (let j = 0; j < maxIterations; j++) {
-            const affinities = calculateAffinities(X, sigmas[i]);
+            const affinities = calculateAffinities(X, sigmas);
             const currentPerplexity = calculatePerplexity(affinities[i]);
 
             if (Math.abs(perp - currentPerplexity) <= 1e-6) {
@@ -52,6 +100,68 @@ export function findBestSigmas(X: number[][], perp: number, maxIterations: numbe
     }
 
     return sigmas;
+}
+
+export function initRandomProjection(nSamples: number, nDims: number): number[][] {
+    const Y: number[][] = new Array(nSamples);
+
+    for (let i = 0; i < nSamples; i++) {
+        Y[i] = new Array(nDims);
+        for (let j = 0; j < nDims; j++) {
+            Y[i][j] = Math.random() / 1e2 - 5e-3;
+        }
+    }
+
+    return Y;
+}
+
+export function calculateGradient(affinities: number[][], Y: number[][]): number[][] {
+    const gradient = new Array(Y.length);
+    const projectedAffinities = calculateProjectedAffinity(Y);
+
+    for (let i = 0; i < Y.length; i++) {
+        gradient[i] = new Array(Y[i].length).fill(0);
+
+        for (let j = 0; j < Y.length; j++) {
+            const coef =
+                (affinities[i][j] - projectedAffinities[i][j]) *
+                (1 + euclideanDistanceSquared(Y[i], Y[j]));
+
+            for (let k = 0; k < Y[i].length; k++) {
+                gradient[i][k] += 4 * coef * (Y[i][k] - Y[j][k]);
+            }
+        }
+    }
+
+    return gradient;
+}
+
+export function calculateProjectedAffinity(Y: number[][]): number[][] {
+    const affinities = new Array(Y.length);
+    const affinitySums = new Array(Y.length).fill(0);
+
+    for (let i = 0; i < Y.length; i++) {
+        affinities[i] = new Array(Y.length).fill(0);
+
+        for (let j = 0; j < Y.length; j++) {
+            if (i === j) {
+                continue;
+            }
+
+            const distance = euclideanDistanceSquared(Y[i], Y[j]);
+            const affinity = Math.pow(1 + distance, -1);
+            affinities[i][j] = affinity;
+            affinitySums[i] += affinity;
+        }
+    }
+
+    for (let i = 0; i < Y.length; i++) {
+        for (let j = 0; j < Y.length; j++) {
+            affinities[i][j] /= affinitySums[i];
+        }
+    }
+
+    return affinities;
 }
 
 export function calculatePerplexity(affinities: number[]): number {
